a543f1de0303f36a65cf7b6e096342c44bc1fdbb
mopedcow <188090362+mopedcow@users.noreply.github.com>
2025-09-28 15:46:08 +0100

    Fix: 'add todo' clears list of projects on start

--- src/DOMcontrol.js
+++ src/DOMcontrol.js
@@ -2,13 +2,25 @@ import { Handler } from "./logicHandler.js";
 
 
 export function displayController() {
-    console.log('top of domcontroller!!!');
+    //console.log('top of domcontroller!!!');
 
     const handler = Handler();
     const mainDisplay = document.querySelector('#content');
 
+    function wipeProjects() {
+        const projects = document.querySelectorAll('.project-container');
+        for (let i = projects.length-1; i >= 0; i--) {
+            projects[i].remove();
+        }
+    }
+
+    function resetProjects() {
+        wipeProjects();
+        generateProjects();
+    }
+
     function generateProjects() {
-        console.log('top of generate projects!!!!!');
+        //console.log('top of generate projects!!!!!');
         let sortedProjects = handler.sortArrayByPriority(handler.projects);
         let sortedTodos = handler.sortArrayByPriority(handler.todos);
 
@@ -28,6 +40,8 @@ export function displayController() {
                 let todoTitle = document.createElement('h2');
                 todoTitle.textContent = todo.title;
 
+                //check if description present
+                //  if none, don't generate anything
                 let todoDesc = document.createElement('p');
                 todoDesc.textContent = `Description: ${todo.desc}`;
 
@@ -36,24 +50,24 @@ export function displayController() {
                 let todoDueDate = document.createElement('span');
                 todoDueDate.textContent = `Due: ${todo.dueDate}`;
 
-                //for todo.priority, change color of todo based on number
-                //ALSO display high, medium, or low.
+                //for todo.priority, change color of todo based on number [not added yet]
                 let todoPriority = document.createElement('div');
+                //ALSO display high, medium, or low:
                 todoPriority.textContent = todo.getPriorityByName();
                 
-                //for status, let user toggle
+                //for status, let user toggle:
                 let todoStatus = document.createElement('button');
                 todoStatus.textContent = `Complete?: ${todo.isDone}`;
                 todoStatus.classList.add('toggle-status-btn');
                 todoStatus.id = todo.todoID;
 
-                //delete todo button
+                //delete todo button:
                 let delTodoBtn = document.createElement('button');
                 delTodoBtn.textContent = 'delete';
                 delTodoBtn.classList.add('todo-del-btn');
                 delTodoBtn.id = todo.todoID;
 
-                //edit todo button
+                //edit todo button:
                 let editTodoBtn = document.createElement('button');
                 editTodoBtn.textContent = 'edit';
                 editTodoBtn.classList.add('todo-edit-btn');
@@ -96,16 +110,13 @@ export function displayController() {
     }
 
     const addTodoDialog = document.querySelector('#add-todo-dialog');
-    const addProjectDialog = document.querySelector('#add-project-dialog');
-
     const addTodoBtn = document.querySelector('#add-todo-btn');
-    const addProjectBtn = document.querySelector('#add-project-btn');
-
     const submitTodoBtn = document.querySelector('#submit-todo-button');
-    const submitProjectBtn = document.querySelector('#submit-project-button');
 
     addTodoBtn.addEventListener('click', () => {
+        //generate projects as options in dropdown
         let projectDropdown = document.querySelector('#select-todo-project');
+        projectDropdown.options.length = 0;
         handler.projects.forEach( (project) => {
             let option = document.createElement('option');
             option.text = project.title;
@@ -117,6 +128,12 @@ export function displayController() {
     })
 
     submitTodoBtn.addEventListener('click', () => {
+        addTodo();
+        resetProjects();
+        addTodoDialog.close();
+    })
+
+    function addTodo() {
         const title = document.getElementById('todo-title').value;
         const desc = document.getElementById('desc').value;
         const dueDate = document.getElementById('due-date').value;
@@ -125,44 +142,27 @@ export function displayController() {
         const projectID = document.getElementById('select-todo-project').value;
 
         handler.createTodo(title, desc, [], dueDate, priority, isDone, projectID);
+    }
 
-        resetProjects();
-        addTodoDialog.close();
-    })
+    const addProjectDialog = document.querySelector('#add-project-dialog');
+    const addProjectBtn = document.querySelector('#add-project-btn');
+    const submitProjectBtn = document.querySelector('#submit-project-button');
 
     addProjectBtn.addEventListener('click', () => {
         addProjectDialog.showModal();
-        let projectScreen = document.querySelector('#add-project-container');
-        let testDiv = document.createElement('div');
-        testDiv.textContent = 'HELLO!';
-        projectScreen.appendChild(testDiv);
     })
 
     submitProjectBtn.addEventListener('click', (e) => {
         //e.preventDefault();
-        const title = document.getElementById('project-title').value;
-        const priority = document.querySelector('input[name="project-priority"]:checked').value;
-
-        handler.createProject(title, priority);
+        addProject();
         resetProjects();
-
         addProjectDialog.close();
     })
 
-    function wipeProjects() {
-        const projects = document.querySelectorAll('.project-container');
-        for (let i = projects.length-1; i >= 0; i--) {
-            projects[i].remove();
-        }
-    }
-
-    function resetProjects() {
-        wipeProjects();
-        generateProjects();
-    }
-
-    function addTodo() {
-
+    function addProject() {
+        const title = document.getElementById('project-title').value;
+        const priority = document.querySelector('input[name="project-priority"]:checked').value;
+        handler.createProject(title, priority);
     }
 
     function editTodo(todoID) {
